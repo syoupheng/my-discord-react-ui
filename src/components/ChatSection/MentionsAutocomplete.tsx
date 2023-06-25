9d95af38efeff15b64768c2b615a565421e7fd2a
syoupheng <do.syoupheng@gmail.com>
2023-06-25 13:48:13 +0200

    Style scrollbar for mentions autocomplete

--- src/components/ChatSection/MentionsAutocomplete.tsx
+++ src/components/ChatSection/MentionsAutocomplete.tsx
@@ -19,8 +19,8 @@ const MentionsAutocomplete = ({ mentionSearch, mentions, arrowPosition, slateVal
       className="bg-secondary absolute left-0 right-0 rounded-[5px] overflow-hidden shadow-[0_0_0_1px_rgba(5,5,6,0.15)] whitespace-nowrap text-ellipsis z-10 pb-0"
       style={{ bottom: `${8 + (1 + slateValue.length) * 22}px` }}
     >
-      <div className="overflow-x-hidden overflow-y-scroll max-h-[490px] pr-0 pb-2 flex flex-col relative min-h-0">
-        <div className="rounded-[3px] p-2">
+      <div className="overflow-x-hidden overflow-y-scroll max-h-[490px] pr-0 pb-2 flex flex-col relative min-h-0 small-scroll-container">
+        <div className="rounded-[3px] p-2 pr-0">
           <h3 className="uppercase py-1 text-h-secondary text-xs font-semibold">
             {mentionSearch.length > 0 ? (
               <>
