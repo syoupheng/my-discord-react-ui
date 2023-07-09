49db91787c91b7e787c316fa43c060ff19ad3998
Pheng <do.syoupheng@gmail.com>
2023-07-10 00:56:45 +0200

    Add POM for auth form e2e tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,76 +1,79 @@
 import { test, expect } from "@playwright/test";
+import { AuthForm } from "../playwright/auth-form";
 
 // test.use({ storageState: undefined });
 
+let authForm: AuthForm;
+
 test.beforeEach(async ({ page }) => {
+  authForm = new AuthForm(page);
   await page.goto("/");
   await expect(page).toHaveURL("/login");
 });
 
 test.describe("email input validation", () => {
-  test("should display an error message when the email is empty", async ({ page, context }) => {
-    console.log("context", await context.storageState());
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-email")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-email")).toContainText("Veuillez renseigner votre email");
+  test("should display an error message when the email is empty", async () => {
+    await authForm.submit();
+    await expect(authForm.emailLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.emailLabel).toContainText("Veuillez renseigner votre email");
   });
 
-  test("should display an error message when the email is invalid", async ({ page }) => {
-    await page.getByLabel("E-mail").fill("invalid-email");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-email")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-email")).toContainText("Veuillez renseigner une adresse email valide");
+  test("should display an error message when the email is invalid", async () => {
+    await authForm.fillEmail("invalid-email");
+    await authForm.submit();
+    await expect(authForm.emailLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.emailLabel).toContainText("Veuillez renseigner une adresse email valide");
   });
 
-  test("should not display an error message when the email is valid", async ({ page }) => {
-    await page.getByLabel("E-mail").fill("some-email@gmail.com");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-email")).not.toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-email")).not.toContainText("Veuillez renseigner une adresse email valide");
+  test("should not display an error message when the email is valid", async () => {
+    await authForm.fillEmail("some-email@gmail.com");
+    await authForm.submit();
+    await expect(authForm.emailLabel).not.toHaveClass(AuthForm.errorClass);
+    await expect(authForm.emailLabel).not.toContainText("Veuillez renseigner une adresse email valide");
   });
 });
 
 test.describe("password input validation", () => {
-  test("should display an error message when the password is empty", async ({ page }) => {
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-password")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-password")).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
+  test("should display an error message when the password is empty", async () => {
+    await authForm.submit();
+    await expect(authForm.passwordLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.passwordLabel).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
   });
 
-  test("should display an error message when the password is too short", async ({ page }) => {
-    await page.getByLabel("Mot de passe").fill("1234567");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-password")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-password")).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
+  test("should display an error message when the password is too short", async () => {
+    await authForm.fillPassword("1234567");
+    await authForm.submit();
+    await expect(authForm.passwordLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.passwordLabel).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
   });
 
-  test("should not display an error message when the password is valid", async ({ page }) => {
-    await page.getByLabel("Mot de passe").fill("12345678");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-password")).not.toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-password")).not.toContainText("Votre mot de passe doit contenir au moins 8 caractères");
+  test("should not display an error message when the password is valid", async () => {
+    await authForm.fillPassword("12345678");
+    await authForm.submit();
+    await expect(authForm.passwordLabel).not.toHaveClass(AuthForm.errorClass);
+    await expect(authForm.passwordLabel).not.toContainText("Votre mot de passe doit contenir au moins 8 caractères");
   });
 });
 
 test("should display an error message when the user email does not exist", async ({ page }) => {
-  await page.getByLabel("E-mail").fill("userThatDoesNotExist@gmail.com");
-  await page.getByLabel("Mot de passe").fill("password");
-  await page.getByRole("button", { name: "Connexion" }).click();
+  await authForm.fillEmail("userThatDoesNotExist@gmail.com");
+  await authForm.fillPassword("password");
+  await authForm.submit();
   await expect(page.getByText("Aucun utilisateur n'existe avec cet email")).toBeVisible();
   await expect(page).not.toHaveURL("/channels/@me");
 });
 
 test("should display an error when wrong password", async ({ page }) => {
-  await page.getByLabel("E-mail").fill("userThatExists@gmail.com");
-  await page.getByLabel("Mot de passe").fill("wrong-password");
-  await page.getByRole("button", { name: "Connexion" }).click();
+  await authForm.fillEmail("userThatExists@gmail.com");
+  await authForm.fillPassword("wrong-password");
+  await authForm.submit();
   await expect(page.getByText("Identifiant ou mot de passe invalide.")).toBeVisible();
   await expect(page).not.toHaveURL("/channels/@me");
 });
 
 test("should redirect to home page when the user is successfully logged in", async ({ page }) => {
-  await page.getByLabel("E-mail").fill("userThatExists@gmail.com");
-  await page.getByLabel("Mot de passe").fill("password");
-  await page.getByRole("button", { name: "Connexion" }).click();
+  await authForm.fillEmail("userThatExists@gmail.com");
+  await authForm.fillPassword("password");
+  await authForm.submit();
   await expect(page).toHaveURL("/channels/@me");
 });
--- e2e/register.spec.ts
+++ e2e/register.spec.ts
@@ -1,85 +1,88 @@
 import { test, expect } from "@playwright/test";
+import { AuthForm } from "../playwright/auth-form";
 
 // test.use({ storageState: undefined });
 
+let authForm: AuthForm;
+
 test.beforeEach(async ({ page }) => {
+  authForm = new AuthForm(page);
   await page.goto("/register");
   await expect(page).toHaveURL("/register");
 });
 
 test.describe("email input validation", () => {
-  test("should display an error message when the email is empty", async ({ page, context }) => {
-    console.log("context", await context.storageState());
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-email")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-email")).toContainText("Veuillez renseigner votre email");
+  test("should display an error message when the email is empty", async () => {
+    await authForm.submit();
+    await expect(authForm.emailLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.emailLabel).toContainText("Veuillez renseigner votre email");
   });
 
-  test("should display an error message when the email is invalid", async ({ page }) => {
-    await page.getByLabel("E-mail").fill("invalid-email");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-email")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-email")).toContainText("Veuillez renseigner une adresse email valide");
+  test("should display an error message when the email is invalid", async () => {
+    await authForm.fillEmail("invalid-email");
+    await authForm.submit();
+    await expect(authForm.emailLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.emailLabel).toContainText("Veuillez renseigner une adresse email valide");
   });
 
-  test("should not display an error message when the email is valid", async ({ page }) => {
-    await page.getByLabel("E-mail").fill("some-email@gmail.com");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-email")).not.toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-email")).not.toContainText("Veuillez renseigner une adresse email valide");
+  test("should not display an error message when the email is valid", async () => {
+    await authForm.fillEmail("some-email@gmail.com");
+    await authForm.submit();
+    await expect(authForm.emailLabel).not.toHaveClass(AuthForm.errorClass);
+    await expect(authForm.emailLabel).not.toContainText("Veuillez renseigner une adresse email valide");
   });
 });
 
 test.describe("username input validation", () => {
-  test("should display an error message when the username is empty", async ({ page }) => {
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-username")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-username")).toContainText("Veuillez renseigner votre nom d'utilisateur");
+  test("should display an error message when the username is empty", async () => {
+    await authForm.submit();
+    await expect(authForm.usernameLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.usernameLabel).toContainText("Veuillez renseigner votre nom d'utilisateur");
   });
 
-  test("should not display an error message when the username is valid", async ({ page }) => {
-    await page.getByLabel("Nom d'utilisateur").fill("some-username");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-username")).not.toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-username")).not.toContainText("Veuillez renseigner votre nom d'utilisateur");
+  test("should not display an error message when the username is valid", async () => {
+    await authForm.fillUsername("some-username");
+    await authForm.submit();
+    await expect(authForm.usernameLabel).not.toHaveClass(AuthForm.errorClass);
+    await expect(authForm.usernameLabel).not.toContainText("Veuillez renseigner votre nom d'utilisateur");
   });
 });
 
 test.describe("password input validation", () => {
-  test("should display an error message when the password is empty", async ({ page }) => {
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-password")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-password")).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
+  test("should display an error message when the password is empty", async () => {
+    await authForm.submit();
+    await expect(authForm.passwordLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.passwordLabel).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
   });
 
-  test("should display an error message when the password is too short", async ({ page }) => {
-    await page.getByLabel("Mot de passe").fill("1234567");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-password")).toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-password")).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
+  test("should display an error message when the password is too short", async () => {
+    await authForm.fillPassword("1234567");
+    await authForm.submit();
+    await expect(authForm.passwordLabel).toHaveClass(AuthForm.errorClass);
+    await expect(authForm.passwordLabel).toContainText("Votre mot de passe doit contenir au moins 8 caractères");
   });
 
-  test("should not display an error message when the password is valid", async ({ page }) => {
-    await page.getByLabel("Mot de passe").fill("12345678");
-    await page.getByRole("button", { name: "Connexion" }).click();
-    await expect(page.getByTestId("label-password")).not.toHaveClass(/text-danger/);
-    await expect(page.getByTestId("label-password")).not.toContainText("Votre mot de passe doit contenir au moins 8 caractères");
+  test("should not display an error message when the password is valid", async () => {
+    await authForm.fillPassword("12345678");
+    await authForm.submit();
+    await expect(authForm.passwordLabel).not.toHaveClass(AuthForm.errorClass);
+    await expect(authForm.passwordLabel).not.toContainText("Votre mot de passe doit contenir au moins 8 caractères");
   });
 });
 
 test("should display an error message when the user already exists", async ({ page }) => {
-  await page.getByLabel("E-mail").fill("userThatExists@gmail.com");
-  await page.getByLabel("Nom d'utilisateur").fill("userThatExists");
-  await page.getByLabel("Mot de passe").fill("12345678");
-  await page.getByRole("button", { name: "Connexion" }).click();
+  await authForm.fillEmail("userThatExists@gmail.com");
+  await authForm.fillUsername("userThatExists");
+  await authForm.fillPassword("12345678");
+  await authForm.submit();
   await expect(page.getByText("Cet email est déjà pris !")).toBeVisible();
 });
 
 test("should successfully register the user", async ({ page }) => {
-  await page.getByLabel("E-mail").fill("newUser@gmail.com");
-  await page.getByLabel("Nom d'utilisateur").fill("newUser");
-  await page.getByLabel("Mot de passe").fill("12345678");
-  await page.getByRole("button", { name: "Connexion" }).click();
+  await authForm.fillEmail("newUser@gmail.com");
+  await authForm.fillUsername("newUser");
+  await authForm.fillPassword("12345678");
+  await authForm.submit();
   await expect(page).toHaveURL("/channels/@me");
   await expect(page.getByText("Bienvenue sur mon clone de Discord !")).toBeVisible();
 });
