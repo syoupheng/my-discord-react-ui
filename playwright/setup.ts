de588ed0605b43d42c1c3b5c8becf2ac5fcb59e9
Pheng <do.syoupheng@gmail.com>
2023-12-18 19:37:42 +0100

    Debug e2e tests

--- playwright/setup.ts
+++ playwright/setup.ts
@@ -6,14 +6,14 @@ export async function clearAndSeedDatabase(request: APIRequestContext) {
       seedDatabase
     }
   `;
-  console.log("API_URL", process.env.VITE_API_URL);
-  console.log("BASE_URL", process.env.BASE_URL);
   const response = await request.post(process.env.VITE_API_URL ?? "http://localhost:3500/graphql", {
     headers: { "Content-Type": "application/json" },
     data: JSON.stringify({
       query,
     }),
   });
+
+  console.log("response", response);
 
   expect(response.ok()).toBeTruthy();
   expect(await response.json()).toEqual({
