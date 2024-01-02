d882ac13a5ee14d29c06a661c1952b1215e0d607
Pheng <do.syoupheng@gmail.com>
2024-01-02 01:35:09 +0100

    Set env variables in test setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -3,7 +3,11 @@ import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import { server } from "@/tests/mocks/server";
 
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+  import.meta.env.VITE_API_URL = "http://localhost:3500/graphql";
+  import.meta.env.VITE_SUBSCRIPTION_URL = "ws://localhost:3500/graphql";
+});
 
 afterAll(() => server.close());
 
